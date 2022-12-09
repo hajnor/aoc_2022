18ce7f11062d4a36ad043f8f2b0151c8fc23a692
Nóra Hajdú <norii.hajdu@gmail.com>
2022-12-09 13:13:15 +0100

    Clean solution

--- challenges/day 8/aoc_1.ts
+++ challenges/day 8/aoc_1.ts
@@ -1,81 +1,72 @@
 import { readFileSync } from 'fs';
 
-printResult(getInput());
+class Solution {
+    private grid: number[][];
+    private readonly visibleTrees: Record<string, string> = {};
 
-function getInput(): string[] {
-    const file = readFileSync('./input.txt', 'utf-8');
+    constructor(input: string[]) {
+        this.printVisibleTreeAmount(input);
+    }
+
+    private printVisibleTreeAmount(input: string[]): void {
+        this.setGrid(input);
+        console.log(this.getVisibleTreeAmount());
+    }
+
+    private setGrid(input: string[]): void {
+        this.grid = input.map(line => Array.from(line).map(Number));
+    }
+
+    private getVisibleTreeAmount(): number {
+        for (let y = 0; y <= this.grid.length - 1; y++) {
+            let max = -1;
+            for (let x = 0; x <= this.grid[0].length - 1; x++) {
+                max = this.getHigherTreeAndAddItToVisibleOnes(y, x, max);
+            }
+        }
+
+        for (let y = 0; y <= this.grid.length - 1; y++) {
+            let max = -1;
+            for (let x = this.grid[0].length - 1; x >= 0; x--) {
+                max = this.getHigherTreeAndAddItToVisibleOnes(y, x, max);
+            }
+        }
+
+        for (let x = 0; x <= this.grid[0].length - 1; x++) {
+            let max = -1;
+            for (let y = 0; y <= this.grid.length - 1; y++) {
+                max = this.getHigherTreeAndAddItToVisibleOnes(y, x, max);
+            }
+        }
+
+
+        for (let x = 0; x <= this.grid[0].length - 1; x++) {
+            let max = -1;
+            for (let y = this.grid.length - 1; y >= 0; y--) {
+                max = this.getHigherTreeAndAddItToVisibleOnes(y, x, max);
+            }
+        }
+
+        return Object.keys(this.visibleTrees).length;
+    }
+
+    private getHigherTreeAndAddItToVisibleOnes(y: number, x: number, highestTree: number): number {
+        const currentTree = this.grid[y][x];
+        if (currentTree > highestTree) {
+            const id = `${y}:${x}`;
+            if (!this.visibleTrees[id]) {
+                this.visibleTrees[id] = id;
+            }
+            return currentTree;
+        }
+        return highestTree;
+    }
+
+}
+
+function getInput(path: string): string[] {
+    const file = readFileSync(path, 'utf-8');
     return file.split(/\r?\n/);
 }
 
-function printResult(input: string[]): void {
-    const trees = input.map(line => Array.from(line).map(Number));
-    console.log(getVisibleTrees(trees));
-}
-
-function getVisibleTrees(trees: number[][]): number {
-    let localMaxes: Record<string, string> = {};    
-    let totalVisibleTrees = 0;
-
-    for (let y = 0; y <= trees.length - 1; y++) {
-        let max = -1;
-        for (let x = 0; x <= trees[0].length - 1; x++) {
-            const currentTree = trees[y][x];
-            const id = `${y}:${x}`;
-            if (currentTree > max) {
-                if (!localMaxes[id]) {
-                    localMaxes[id] = id;
-                    totalVisibleTrees++;
-                }
-                max = currentTree;
-            }
-        }
-    }
-
-    for (let y = 0; y <= trees.length - 1; y++) {
-        let max = -1;
-        for (let x = trees[0].length - 1; x >= 0; x--) {
-            const currentTree = trees[y][x];
-            const id = `${y}:${x}`;
-            if (currentTree > max) {
-                if (!localMaxes[id]) {
-                    localMaxes[id] = id;
-                    totalVisibleTrees++;
-                }
-                max = currentTree;
-            }
-        }
-    }
-
-    for (let x = 0; x <= trees[0].length - 1; x++) {
-        let max = -1;
-        for (let y = 0; y <= trees.length - 1; y++) {
-            const currentTree = trees[y][x];
-            const id = `${y}:${x}`;
-            if (currentTree > max) {
-                if (!localMaxes[id]) {
-                    localMaxes[id] = id;
-                    totalVisibleTrees++;
-                }
-                max = currentTree;
-            }
-        }
-    }
-
-
-    for (let x = 0; x <= trees[0].length - 1; x++) {
-        let max = -1;
-        for (let y = trees.length - 1; y >= 0; y--) {
-            const currentTree = trees[y][x];
-            const id = `${y}:${x}`;
-            if (currentTree > max) {
-                if (!localMaxes[id]) {
-                    localMaxes[id] = id;
-                    totalVisibleTrees++;
-                }
-                max = currentTree;
-            } 
-        }
-    }
-
-    return totalVisibleTrees;
-}
+new Solution(getInput('./input.txt'));
